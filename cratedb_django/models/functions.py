2856e22e14663d5d7a57bbca51d505cfd442a5df
surister <surister98@gmail.com>
2025-12-16 21:06:07 +0100

    fix: Make `UUID` work as an expression by setting its `output_field`.

--- cratedb_django/models/functions.py
+++ cratedb_django/models/functions.py
@@ -1,5 +1,7 @@
 from django.db.models.expressions import Func
 
+from cratedb_django.fields import TextField
+
 """
 class SomeModel(CrateModel):
     id = models.TextField(primary_key=True, db_default=UUID())
@@ -11,3 +13,4 @@ class UUID(Func):
     """https://cratedb.com/docs/crate/reference/en/latest/general/builtins/scalar-functions.html#gen-random-text-uuid"""
 
     function = "gen_random_text_uuid"
+    output_field = TextField(max_length=20)  # the length of a CrateDB random uid.
